b583da64187b93298e93ae2fe1f92cf242615fc8
MoltenCan <>
2022-01-15 17:04:56 -0800

    adding envs

--- printbox/ep.sh
+++ printbox/ep.sh
@@ -5,20 +5,27 @@
     exit 0
 }
 
-[ -e /var/run/docker.sock ] || {
-    echo "no /var/run/docker.sock"
-    exit 1
+function check_sock {
+    [ -e /var/run/docker.sock ] || {
+        echo "no /var/run/docker.sock"
+        exit 1
+    }
 }
 
 case "$1" in
 up)
+    check_sock
     printbox || exit 1
     docker-compose -f /printbox/docker-compose.yml up -d
     ;;
 down)
+    check_sock
     docker-compose -f /printbox/docker-compose.yml down
     ;;
+list)
+    printbox
+    ;;
 *)
-    echo "sh | up | down"
+    echo "sh | up | down | list"
     ;;
 esac
